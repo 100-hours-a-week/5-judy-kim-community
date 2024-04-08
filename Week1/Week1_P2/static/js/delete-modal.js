6a1a56a8b4579aa3cec6eefd38b517bebad49ed5
judy-kimsohui <sohuikim2020@naver.com>
2024-04-08 14:14:38 +0900

    add modal

---
+++ Week1/Week1_P2/static/js/delete-modal.js
@@ -0,0 +1,50 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // elements
+    var modalBtn = document.getElementById("edit-button-delete");
+    var modal = document.getElementById("edit-button-modal");
+    var cancelBtn = document.getElementById("edit-cancel");
+    var deleteBtn = document.getElementById("edit-delete");
+  
+    // functions
+    function toggleModal() {
+      modal.classList.toggle("show");
+    }
+  
+    // events
+    modalBtn.addEventListener("click", toggleModal);
+    cancelBtn.addEventListener("click", toggleModal);
+    deleteBtn.addEventListener("click", toggleModal);
+  
+    window.addEventListener("click", function (event) {
+        // 모달의 검은색 배경 부분이 클릭된 경우 닫히도록 하는 코드
+        if (event.target === modal) {
+            toggleModal();
+        }
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    // elements
+    var modalBtn = document.getElementById("command-button-delete");
+    var modal = document.getElementById("command-button-modal");
+    var cancelBtn = document.getElementById("command-cancel");
+    var deleteBtn = document.getElementById("command-delete");
+  
+    // functions
+    function toggleModal() {
+      modal.classList.toggle("show");
+    }
+  
+    // events
+    modalBtn.addEventListener("click", toggleModal);
+    cancelBtn.addEventListener("click", toggleModal);
+    deleteBtn.addEventListener("click", toggleModal);
+  
+    window.addEventListener("click", function (event) {
+        // 모달의 검은색 배경 부분이 클릭된 경우 닫히도록 하는 코드
+        if (event.target === modal) {
+            toggleModal();
+        }
+    });
+});
