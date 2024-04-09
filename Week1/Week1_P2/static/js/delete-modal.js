b2e4b7b86ceb56f9a09e2a2eff5becbad0a9fa30
judy-kimsohui <sohuikim2020@naver.com>
2024-04-09 11:14:20 +0900

    succ p2

--- Week1/Week1_P2/static/js/delete-modal.js
+++ Week1/Week1_P2/static/js/delete-modal.js
@@ -16,7 +16,6 @@ document.addEventListener("DOMContentLoaded", function () {
     deleteBtn.addEventListener("click", toggleModal);
   
     window.addEventListener("click", function (event) {
-        // 모달의 검은색 배경 부분이 클릭된 경우 닫히도록 하는 코드
         if (event.target === modal) {
             toggleModal();
         }
@@ -25,26 +24,29 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    // elements
-    var modalBtn = document.getElementById("command-button-delete");
-    var modal = document.getElementById("command-button-modal");
-    var cancelBtn = document.getElementById("command-cancel");
-    var deleteBtn = document.getElementById("command-delete");
-  
-    // functions
-    function toggleModal() {
-      modal.classList.toggle("show");
-    }
-  
-    // events
-    modalBtn.addEventListener("click", toggleModal);
-    cancelBtn.addEventListener("click", toggleModal);
-    deleteBtn.addEventListener("click", toggleModal);
-  
+    document.querySelectorAll('.command-button-delete').forEach(function (modalBtn, index) {
+        var modal = document.getElementById(`command-button-modal-${index + 1}`);
+        
+        modalBtn.addEventListener("click", function () {
+            modal.classList.toggle("show");
+        });
+
+        modal.querySelector('.command-cancel').addEventListener("click", function () {
+            modal.classList.remove("show");
+        });
+
+        modal.querySelector('.command-delete').addEventListener("click", function () {
+            modal.classList.remove("show");
+        });
+    });
+
     window.addEventListener("click", function (event) {
-        // 모달의 검은색 배경 부분이 클릭된 경우 닫히도록 하는 코드
-        if (event.target === modal) {
-            toggleModal();
-        }
+        document.querySelectorAll('.command-delete-modal').forEach(function (modal) {
+            if (event.target === modal) {
+                modal.classList.remove("show");
+            }
+        });
     });
 });
+
+
