6fcccc94c25f53cf1ce6768b8cb79805304e3314
judy-kimsohui <sohuikim2020@naver.com>
2024-04-17 14:05:06 +0900

    check button color signup

--- Week2/Week2_P2/static/js/availability.js
+++ Week2/Week2_P2/static/js/availability.js
@@ -1,29 +1,48 @@
 document.addEventListener('DOMContentLoaded', (event) => {    
 
-    // signup
-    // 프로필 이미지 검증
+    // 요소 참조
     let inputProfileImage = document.querySelector('#inputProfileImage');   
     let helpProfileImage = document.querySelector('.helptext.profileImage');
+    let inputEmail = document.querySelector('#inputEmail');     
+    let helpEmail = document.querySelector('.helptext.email');
+    let inputPassword = document.querySelector('#inputPassword');     
+    let helpPassword = document.querySelector('.helptext.password');  
+    let inputRetypePassword = document.querySelector('#inputRetypePassword');
+    let helpRetypePassword = document.querySelector('.helptext.retypePassword');
+    let inputNickname = document.querySelector('#inputNickname'); 
+    let helpNickname = document.querySelector('.helptext.nickname');
+    
+    // 중복 이메일 예시 데이터
+    const existingEmails = ['example@example.com', 'test@test.com'];
+    const existingNicknames = ['nickname1', 'nickname2'];
 
+    let inputProfileImageValid = false;
+    let inputEmailValid = false;
+    let inputPasswordValid = false;
+    let inputRetypePasswordValid = false;
+    let inputNicknameValid = false;
+
+    updateSignupButtonStatus();
+
+    // signup
+    // 프로필 이미지 검증
     inputProfileImage.onchange = function () {
         helpProfileImage.classList.remove('hide');
         if (inputProfileImage.files.length > 0) {
             helpProfileImage.textContent = "* 프로필 사진이 업로드 되었습니다.";
             helpProfileImage.style.color = "green";
+            inputProfileImageValid = true;
         } else {
             helpProfileImage.textContent = "* 프로필 사진을 추가해주세요.";
             helpProfileImage.style.color = "red";
+            inputProfileImageValid = false;
         }
+
+        updateSignupButtonStatus();
     };
 
     // signup
     // 이메일 검증
-    let inputEmail = document.querySelector('#inputEmail');     
-    let helpEmail = document.querySelector('.helptext.email');     
-
-    // 중복 이메일 예시 데이터
-    const existingEmails = ['example@example.com', 'test@test.com'];
-
     inputEmail.onkeyup = function () {
         helpEmail.classList.remove('hide');
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -31,89 +50,126 @@ document.addEventListener('DOMContentLoaded', (event) => {
             if (existingEmails.includes(inputEmail.value)) {
                 helpEmail.textContent = "* 중복된 이메일입니다.";
                 helpEmail.style.color = "red";
+                inputEmailValid = false;
             } else {
                 helpEmail.textContent = "* 사용할 수 있는 이메일 입니다.";
                 helpEmail.style.color = "green";
+                inputEmailValid = true;
             }
         } else {
             helpEmail.textContent = "* 올바른 이메일 형식을 입력해주세요.";
             helpEmail.style.color = "red";
+            inputEmailValid = false;
         }
+
+        updateSignupButtonStatus();
     };    
 
     // signup, profile-edit2
     // 비밀번호 검증
-    let inputPassword = document.querySelector('#inputPassword');     
-    let helpPassword = document.querySelector('.helptext.password');  
-
     inputPassword.onkeyup = function () {
         helpPassword.classList.remove('hide');
         if (inputPassword.value.length > 0) {
             if (inputPassword.value.length >= 8 && inputPassword.value.length <= 20) {
                 helpPassword.textContent = "* 사용할 수 있는 비밀번호 입니다.";
                 helpPassword.style.color = "green";
+                inputPasswordValid = true;
             } else {
                 helpPassword.textContent = "* 비밀번호는 8자 이상, 20자 이하이며, 대문자, 소문자, 숫자, 특수 문자를 각각 최소 1개 포함해야 합니다.";
                 helpPassword.style.color = "red";
+                inputPasswordValid = false;
             }
         } else {
             helpPassword.textContent = "* 비밀번호를 입력해주세요.";
             helpPassword.style.color = "red";
+            inputPasswordValid = false;
         }
+
+        if (inputRetypePassword.value.length !== 0) {
+            if (inputRetypePassword.value === inputPassword.value) {
+                helpRetypePassword.textContent = "* 비밀번호가 일치합니다.";
+                helpRetypePassword.style.color = "green";
+                inputRetypePasswordValid = true;
+            } else {
+                helpRetypePassword.textContent = "* 비밀번호가 다릅니다.";
+                helpRetypePassword.style.color = "red";
+                inputRetypePasswordValid = false;
+            }
+        } else {
+            helpRetypePassword.textContent = "* 비밀번호를 한 번 더 입력해주세요.";
+            helpRetypePassword.style.color = "red";
+            inputRetypePasswordValid = false;
+        }
+
+        updateSignupButtonStatus();
     };
 
     // signup, profile-edit2
     // 비밀번호 재입력 검증
-    let inputRetypePassword = document.querySelector('#inputRetypePassword');
-    let helpRetypePassword = document.querySelector('.helptext.retypePassword');
-
     inputRetypePassword.onkeyup = function () {
         helpRetypePassword.classList.remove('hide');
         if (inputRetypePassword.value.length !== 0) {
             if (inputRetypePassword.value === inputPassword.value) {
                 helpRetypePassword.textContent = "* 비밀번호가 일치합니다.";
                 helpRetypePassword.style.color = "green";
+                inputRetypePasswordValid = true;
             } else {
                 helpRetypePassword.textContent = "* 비밀번호가 다릅니다.";
                 helpRetypePassword.style.color = "red";
+                inputRetypePasswordValid = false;
             }
         } else {
             helpRetypePassword.textContent = "* 비밀번호를 한 번 더 입력해주세요.";
             helpRetypePassword.style.color = "red";
+            inputRetypePasswordValid = false;
         }
+
+        updateSignupButtonStatus();
     };
 
 
     // signup, profile-edit1
     // 닉네임 검증
-    let inputNickname = document.querySelector('#inputNickname'); 
-    let helpNickname = document.querySelector('.helptext.nickname');
-    
-    // 중복 닉네임 예시 데이터
-    const existingNicknames = ['nickname1', 'nickname2'];
-
     inputNickname.onkeyup = function () {
         helpNickname.classList.remove('hide');
         if (inputNickname.value.length === 0) {
             helpNickname.textContent = "* 닉네임을 입력해 주세요.";
             helpNickname.style.color = "red";
+            inputNicknameValid = false;
         } else if (inputNickname.value.includes(' ')) {
             helpNickname.textContent = "* 띄어쓰기를 없애주세요.";
             helpNickname.style.color = "red";
+            inputNicknameValid = false;
         } else if (inputNickname.value.length > 10) {
             helpNickname.textContent = "* 닉네임은 최대 10글자까지 작성 가능합니다.";
             helpNickname.style.color = "red";
+            inputNicknameValid = false;
         } else {
             if (existingNicknames.includes(inputNickname.value)) {
                 helpNickname.textContent = "* 중복된 닉네임입니다.";
                 helpNickname.style.color = "red";
+                inputNicknameValid = false;
             } else {
                 helpNickname.textContent = "* 사용할 수 있는 닉네임입니다.";
                 helpNickname.style.color = "green";
+                inputNicknameValid = true;
             }
         }
+
+        updateSignupButtonStatus();
     };
 
+    // 모든 검증을 통합하여 회원 가입 버튼 활성화
+    function updateSignupButtonStatus() {
+        const signupButton = document.querySelector('#signupButton');
+        if (inputEmailValid && inputPasswordValid && inputRetypePasswordValid && inputNicknameValid && inputProfileImageValid) {
+            signupButton.classList.add("active");
+            signupButton.disabled = false;
+        } else {
+            signupButton.classList.remove("active");
+            signupButton.disabled = true;
+        }
+    }
     
     // login
     // 로그인 검증
