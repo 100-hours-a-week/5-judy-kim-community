51d0e9b14cb6592b66bce30d2080003b39745a80
judy-kimsohui <sohuikim2020@naver.com>
2024-07-03 17:58:38 +0900

    update main

---
+++ Week13/spring-main-library/ghcr-main-pjt-main/src/main/java/org/judy/Main.java
@@ -0,0 +1,10 @@
+package org.judy;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        Greeter greeter = new Greeter();
+        System.out.println(greeter.greet("judy"));
+
+    }
+}
